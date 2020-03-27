d368cf548aba3f61c60d903c744a03e12eb61547
Yotam Bar On <yotam.b@taboola.com>
2020-03-27 16:06:54 +0300

    Add amp-taboola extension with PoC for supporting Taboola Next Up

---
+++ extensions/amp-taboola/0.1/amp-taboola.js
@@ -0,0 +1,65 @@
+export class AmpTaboola extends AMP.BaseElement {
+    /** @override */
+    buildCallback() {
+        this._publisher = this.element.getAttribute('publisher');
+        this._pageType = this.element.getAttribute('page-type');
+        this._config = JSON.parse(this.element.textContent);
+
+        Object.entries(this._config).forEach(([placement, config]) => {
+            const container = this.element.ownerDocument.getElementById(config.container);
+
+            const taboolaEmbed = this.createTaboolaEmbed({
+                component: 'embed',
+                placement,
+                mode: config.mode,
+                height: config.height,
+                width: config.width
+            });
+
+            container.appendChild(taboolaEmbed);
+            this.setupAmpEmbedCommunications(taboolaEmbed);
+        });
+    }
+
+    createTaboolaEmbed({ component, placement, mode, height, width }) {
+            const taboolaEmbed = this.element.ownerDocument.createElement(`amp-${component}`);
+            taboolaEmbed.setAttribute('type', 'taboola');
+            taboolaEmbed.setAttribute('height', height);
+            taboolaEmbed.setAttribute('width', width);
+            taboolaEmbed.setAttribute('data-publisher', this._publisher);
+            taboolaEmbed.setAttribute('data-placement', placement);
+            taboolaEmbed.setAttribute('data-mode', mode);
+            taboolaEmbed.setAttribute(`data-${this._pageType}`, 'auto');
+
+            return taboolaEmbed;
+    }
+
+    setupAmpEmbedCommunications(embed) {
+        window.addEventListener('message', ({ data }) => {
+            if (data.taboola) {
+                if (data.loadNextUp) {
+                    this.loadNextUp(data.loadNextUp);
+                }
+            }
+        });
+    }
+
+    loadNextUp(data) {
+        const stickyAd = this.element.ownerDocument.createElement('amp-sticky-ad');
+        stickyAd.setAttribute('layout', 'nodisplay');
+        const taboolaEmbed = this.createTaboolaEmbed({
+            component: 'ad',
+            placement: 'Ads Example',
+            mode: data.mode,
+            height: 100,
+            width: 300
+        });
+
+        stickyAd.appendChild(taboolaEmbed);
+        this.element.ownerDocument.body.appendChild(stickyAd);
+    }
+}
+
+AMP.extension('amp-taboola', '0.1', AMP => {
+  AMP.registerElement('amp-taboola', AmpTaboola);
+});
