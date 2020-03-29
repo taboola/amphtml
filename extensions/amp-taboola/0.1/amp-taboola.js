0bc6a074a8d91088753321579a23e19ae1873c04
Yotam Bar On <yotam.b@taboola.com>
2020-03-29 09:33:25 +0300

    Support Feed View on <amp-taboola/> PoC

--- extensions/amp-taboola/0.1/amp-taboola.js
+++ extensions/amp-taboola/0.1/amp-taboola.js
@@ -13,7 +13,8 @@ export class AmpTaboola extends AMP.BaseElement {
                 placement,
                 mode: config.mode,
                 height: config.height,
-                width: config.width
+                width: config.width,
+                targetType: config.target_type
             });
 
             container.appendChild(taboolaEmbed);
@@ -21,7 +22,7 @@ export class AmpTaboola extends AMP.BaseElement {
         });
     }
 
-    createTaboolaEmbed({ component, placement, mode, responsive = true, height, width }) {
+    createTaboolaEmbed({ component, placement, mode, targetType, responsive = true, height, width }) {
             const taboolaEmbed = this.element.ownerDocument.createElement(`amp-${component}`);
             if (responsive) {
                 taboolaEmbed.setAttribute('layout', 'responsive');
@@ -33,6 +34,9 @@ export class AmpTaboola extends AMP.BaseElement {
             taboolaEmbed.setAttribute('data-placement', placement);
             taboolaEmbed.setAttribute('data-mode', mode);
             taboolaEmbed.setAttribute(`data-${this._pageType}`, 'auto');
+            if (targetType) {
+                taboolaEmbed.setAttribute('data-target_type', targetType);
+            }
 
             return taboolaEmbed;
     }
@@ -46,6 +50,10 @@ export class AmpTaboola extends AMP.BaseElement {
 
                 if (data.loadReadMore) {
                     this.loadReadMore(data.loadReadMore);
+                }
+
+                if (data.loadFeedView) {
+                    this.loadFeedView(data.loadFeedView);
                 }
             }
         });
@@ -87,6 +95,56 @@ export class AmpTaboola extends AMP.BaseElement {
             content.style.overflow = '';
             content.parentNode.removeChild(button);
         });
+    }
+
+    loadFeedView(data) {
+        window.history.pushState(
+            { inFeedView: true },
+            data.title
+        );
+
+        const iframe = this.element.ownerDocument.createElement('amp-iframe');
+        const placeholder = this.element.ownerDocument.createElement('amp-img');
+
+        placeholder.setAttribute('src', 'https://www.taboola.com/sites/all/themes/taboola2017/images/branding/logo-blue/logo-blue.png');
+        placeholder.setAttribute('width', '222');
+        placeholder.setAttribute('height', '54');
+        placeholder.setAttribute('layout', 'fixed');
+        placeholder.style.marginBottom = '128px';
+        placeholder.style.backgroundColor = '#fff';
+
+        const placeholderContainer = this.element.ownerDocument.createElement('div');
+        placeholderContainer.setAttribute('placeholder', '');
+        placeholderContainer.style.backgroundColor = '#fff';
+        placeholderContainer.style.display = 'flex';
+        placeholderContainer.style.alignItems = 'center';
+        placeholderContainer.style.justifyContent = 'center';
+        placeholderContainer.appendChild(placeholder);
+
+        iframe.appendChild(placeholderContainer);
+
+        iframe.style.position = 'fixed';
+        iframe.style.top = '0';
+        iframe.style.left = '0';
+        iframe.style.width = '100%';
+        iframe.style.height = '100vh';
+        iframe.style.backgroundColor = '#fff';
+        iframe.setAttribute('width', '480');
+        iframe.setAttribute('height', '920');
+        iframe.setAttribute('src', data.logger_url);
+        iframe.setAttribute('layout', 'responsive');
+        this.element.ownerDocument.body.appendChild(iframe);
+
+        const handler = () => {
+            const state = window.history.state;
+            if (state.hasOwnProperty('inFeedView')) {
+                this.element.ownerDocument.body.removeChild(iframe);
+                const newState = Object.assign({}, state, { inFeedView: false });
+                window.history.replaceState(newState)
+                window.removeEventListener('popstate', handler);
+            }
+        };
+        window.addEventListener('popstate', handler);
     }
 }
 
